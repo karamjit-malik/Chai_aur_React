7cd9dcc07052b430b784940d86eecfe93746ae78
karamjit-malik <karamjitmalik606@gmail.com>
2025-11-19 21:45:42 +0530

    Refactor currency conversion logic in App component

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -74,7 +74,7 @@ function App() {
             type="submit"
             className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg mt-4"
           >
-            Convert
+            Convert {from.toUpperCase()} to {to.toUpperCase()}
           </button>
         </form>
       </div>
